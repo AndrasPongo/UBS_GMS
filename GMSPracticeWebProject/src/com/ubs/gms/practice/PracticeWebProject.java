71b2cf91c8cee5e14905cef989c0dcb1aaf8c392
AndrasPongo <andras.pongo14@gmail.com>
2018-07-18 20:49:13 +0200

    Init

---
+++ GMSPracticeWebProject/src/com/ubs/gms/practice/PracticeWebProject.java
@@ -0,0 +1,14 @@
+package com.ubs.gms.practice;
+
+import org.apache.logging.log4j.*;
+
+public class PracticeWebProject {
+
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		LOGGER.error("Hello WorldWideWeb");
+	}
+
+}
